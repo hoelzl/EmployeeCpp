004e2186f9ee97bab1551d5a46e6c838b4302780
Matthias Hölzl <tc@xantira.com>
2019-06-13 01:02:48 +0200

    Add unit test for creation of employee with id 7

--- src/EmployeeTest.cpp
+++ src/EmployeeTest.cpp
@@ -1,5 +1,6 @@
 #pragma clang diagnostic push
 #pragma ide diagnostic ignored "cert-err58-cpp"
+
 // Created by Matthias Hölzl on 2019-05-28.
 //
 
@@ -9,14 +10,47 @@
 #include "NameFileReader.h"
 #include "gmock/gmock.h"
 
+using namespace ::testing;
+
+class EmployeeDataReaderStub : public EmployeeDataReader
+{
+public:
+    EmployeeData readEmployeeData(int id) const override
+    {
+        return EmployeeData{7, std::tm{0, 0, 0, 7, 5 /* tm_mon goes from 0 to 11 */, 58, 0, 0, 0}};
+    }
+};
+
+class NameReaderStub : public NameReader
+{
+public:
+    NameData readName(int id) const override
+    {
+        return NameData{"Rogers", "Nelson"};
+    }
+};
+
+class CalendarReaderStub : public CalendarReader
+{
+public:
+    std::map<Time, Location> ReadCalenderData(int employee_id) const override
+    {
+        return std::map<Time, Location>{};
+    }
+};
+
 TEST(Employee, CreateEmployee)
 {
-    EmployeeDataFileReader employee_data_file_reader{"employee-data.txt"};
-    NameFileReader name_file_reader{"employee-data.txt"};
-    CalendarFileReader calendar_file_reader{"employee-calendars.txt"};
+    EmployeeDataReaderStub employee_data_reader{};
+    NameReaderStub name_reader{};
+    CalendarReaderStub calendar_reader{};
+    Employee employee{7, employee_data_reader, name_reader, calendar_reader};
+    std::ostringstream stream{};
+    const std::string result{"Employee{7: Rogers Nelson, 1958-06-07, $100}\n"};
 
-    Employee employee{1, employee_data_file_reader, name_file_reader, calendar_file_reader};
-    ASSERT_TRUE(true);
+    stream << employee;
+
+    ASSERT_THAT(stream.str(), Eq(result));
 }
 
 #pragma clang diagnostic pop
