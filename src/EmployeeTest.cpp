1714a2a6161585a2971afdb0cefeff5d5295a1fe
Matthias Hölzl <tc@xantira.com>
2019-06-13 17:08:14 +0200

    Suppress cert-err58-cpp warning for single test, not entire file
    * Suppressing it for the whole file leads to a different notifier being
      shown in the gutter and therefore does not buy us a lot...

--- src/EmployeeTest.cpp
+++ src/EmployeeTest.cpp
@@ -1,15 +1,12 @@
-#pragma clang diagnostic push
-#pragma ide diagnostic ignored "cert-err58-cpp"
 // Created by Matthias Hölzl on 2019-05-28.
 //
 
 #include "Employee.h"
 #include "gmock/gmock.h"
 
-TEST(Employee, CreateEmployee)
+TEST(Employee, CreateEmployee) // NOLINT(cert-err58-cpp)
 {
     Employee employee{1};
     ASSERT_TRUE(true);
 }
 
-#pragma clang diagnostic pop
